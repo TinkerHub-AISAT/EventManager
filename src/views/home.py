8eb3863c6c95895b60ad657e74251a24b4e9313b
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-11-23 08:07:04 +0530

    add new pages and related routes
    - added a about page
    - added a contact page
    - added all the routes related to the pages

--- src/views/home.py
+++ src/views/home.py
@@ -6,3 +6,13 @@ home_views = Blueprint("home_views", __name__)
 @home_views.route("/")
 def welcome_page():
     return render_template("home/home_welcome.html")
+
+
+@home_views.route("/aboutus")
+def aboutus():
+    return render_template("home/home_about.html")
+
+
+@home_views.route("/contactus")
+def contactus():
+    return render_template("home/home_contact.html")
