591ad2979898df1f056a09220502f62f71da9375
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-11-24 19:54:12 +0530

    just an update

--- src/views/home.py
+++ src/views/home.py
@@ -1,18 +1,18 @@
 from flask import Blueprint, render_template
 
-home_views = Blueprint("home_views", __name__)
+home_views = Blueprint(name="home_views", import_name=__name__)
 
 
-@home_views.route("/")
+@home_views.route(rule="/", methods=["GET"])
 def welcome_page():
     return render_template("home/home_welcome.html")
 
 
-@home_views.route("/aboutus")
+@home_views.route(rule="/aboutus", methods=["GET"])
 def aboutus():
     return render_template("home/home_about.html")
 
 
-@home_views.route("/contactus")
+@home_views.route(rule="/contactus", methods=["GET"])
 def contactus():
     return render_template("home/home_contact.html")
