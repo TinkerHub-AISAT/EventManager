2090a276262572a6b12da3a28ebe401c676e3337
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-11-24 17:57:19 +0530

    Updated files

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,23 @@
+# * Dotenv module
+from dotenv import load_dotenv
 from flask import Flask
+
+# * firebase admin
+# import firebase_admin
+# from firebase_admin import credentials, firestore
+
+
+# custom imports
+# from .config.cred import FirebaseCreds
+
+# load of the env variables in the project environment
+load_dotenv()
 
 
 def flask_app():
     app = Flask(__name__)
+
+    # _FIREBASECRED = FirebaseCreds()
 
     from .views import home
 
