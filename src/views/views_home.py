2bb81f18197807bf7ce1b0168ec9db4ce8ccb737
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-12-02 22:04:47 +0530

    🚀 formatted code

--- src/views/views_home.py
+++ src/views/views_home.py
@@ -5,14 +5,14 @@ HomeViews = Blueprint(name="HomeViews", import_name=__name__)
 
 @HomeViews.route(rule="/", methods=["GET"])
 def welcome_page():
-    return render_template("home/home_welcome.html")
+    return render_template(template_name_or_list="home/home_welcome.html")
 
 
 @HomeViews.route(rule="/aboutus", methods=["GET"])
 def aboutus():
-    return render_template("home/home_about.html")
+    return render_template(template_name_or_list="home/home_about.html")
 
 
 @HomeViews.route(rule="/contactus", methods=["GET"])
 def contactus():
-    return render_template("home/home_contact.html")
+    return render_template(template_name_or_list="home/home_contact.html")
